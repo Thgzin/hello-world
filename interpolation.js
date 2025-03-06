023277a7af19ddda6c2497526f8bf4fa10bf259e
thgzin <thgziin@outlook.com>
2025-03-06 00:18:07 -0300

    asa

--- interpolation.js
+++ interpolation.js
@@ -1,1 +1,11 @@
-console.log(`Total: ${5 + 3}`);
+var name = "My name is:";
+var th = "Thiago";
+console.log(`${name}  ${th}`);
+
+console.log(`Total cost: ${5 + 3}`);
+console.log(`Total cost: $${(599 + 295) / 100}`);
+
+
+
+console.log(`Total cost: ${5 + 3}`);
+console.log(`Total cost: $${(599 + 295) / 100}`);
