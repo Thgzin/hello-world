ec86476b327dee448fa0d26706a4e8bb7fdc11c9
thgzin <thgziin@outlook.com>
2025-03-09 14:33:00 -0300

    somase

--- interpolation.js
+++ interpolation.js
@@ -5,7 +5,11 @@ console.log(`${name}  ${th}`);
 console.log(`Total cost: ${5 + 3}`);
 console.log(`Total cost: $${(599 + 295) / 100}`);
 
-
-
 console.log(`Total cost: ${5 + 3}`);
 console.log(`Total cost: $${(599 + 295) / 100}`);
+
+function soma(s) {
+  return s + 5 * 2;
+}
+
+console.log(soma(5));
