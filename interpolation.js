c5b8fb180f59283c007460bd509f07bb8df9c7b6
thgzin <thgziin@outlook.com>
2025-03-10 16:05:43 -0300

    iadj

--- interpolation.js
+++ interpolation.js
@@ -13,3 +13,19 @@ function soma(s) {
 }
 
 console.log(soma(5));
+
+var name = "My name is:";
+var th = "Thiago";
+console.log(`${name}  ${th}`);
+
+console.log(`Total cost: ${5 + 3}`);
+console.log(`Total cost: $${(599 + 295) / 100}`);
+
+console.log(`Total cost: ${5 + 3}`);
+console.log(`Total cost: $${(599 + 295) / 100}`);
+
+function soma(s) {
+  return s + 5 * 2;
+}
+
+console.log(soma(5));
