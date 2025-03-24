16bfbad169aa8ed16561d5347a8487c42d076fa0
thgzin <thgziin@outlook.com>
2025-03-24 20:19:42 -0300

    fix

---
+++ exercises.js
@@ -0,0 +1,19 @@
+const name = "thiago";
+
+console.log(`My name is: ${name}`);
+
+let coffee = 5;
+let bagels = 2 * 3;
+let soup = 9;
+const cost = coffee + bagels + soup;
+
+console.log(`${coffee}$ coffes, ${bagels}$ bagels, ${soup}$ total = ${cost}`);
+
+console.log(`Cost of food: ${cost}`);
+
+const tax = cost * 0.1;
+console.log(`taxa: ${tax}`);
+
+const total = cost + tax;
+
+console.log(`Total cost: ${total}$`);
