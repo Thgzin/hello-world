5fb63c6d82fe416b6fbd39768c4f5e6f2d2ecea7
Thgzin <thgziin@outlook.com>
2025-05-14 23:45:13 -0300

    acumulattorPatterning2

--- section11/script/todo-list.js
+++ section11/script/todo-list.js
@@ -1,4 +1,17 @@
-const todoList = [];
+const todoList = ["make dinner", "wash dishes", "buy groceries"];
+
+renderTodoList();
+function renderTodoList() {
+  let todoListHtml = "";
+
+  for (let i = 0; i < todoList.length; i++) {
+    const todo = todoList[i];
+    const html = `<p>${todo}</p>`;
+    todoListHtml += html;
+  }
+
+  document.querySelector(".todoText").innerHTML = todoListHtml;
+}
 
 function addTodo() {
   const inputElement = document.querySelector(".todo-input");
@@ -7,4 +20,5 @@ function addTodo() {
   todoList.push(todoName);
   console.log(todoList);
   inputElement.value = "";
+  renderTodoList();
 }
