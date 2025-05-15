f5b50847dbe69291a7502cf7fd014c1e15b5097e
Thgzin <thgziin@outlook.com>
2025-05-15 16:29:51 -0300

    execisesArray

---
+++ section11/Array&ObjectsPart1/script/todo-list.js
@@ -0,0 +1,25 @@
+const todoList = ["make dinner", "wash dishes", "buy groceries"];
+
+renderTodoList();
+
+function renderTodoList() {
+  let todoListHtml = "";
+
+  for (let i = 0; i < todoList.length; i++) {
+    const todo = todoList[i];
+    const html = `<p>${todo}</p>`;
+    todoListHtml += html;
+  }
+
+  document.querySelector(".todoText").innerHTML = todoListHtml;
+}
+
+function addTodo() {
+  const inputElement = document.querySelector(".todo-input");
+  const todoName = inputElement.value;
+
+  todoList.push(todoName);
+  console.log(todoList);
+  inputElement.value = "";
+  renderTodoList();
+}
