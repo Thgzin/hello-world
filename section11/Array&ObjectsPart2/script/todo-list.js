939cb8f4b932811234a217e146889be0a01e7ba7
Thgzin <thgziin@outlook.com>
2025-05-16 11:11:06 -0300

    todo-project

--- section11/Array&ObjectsPart2/script/todo-list.js
+++ section11/Array&ObjectsPart2/script/todo-list.js
@@ -1,10 +1,4 @@
-const todoList = [
-  { name: "make dinner", dueDate: "2025-10-24" },
-  {
-    name: "buy groceries",
-    dueDate: "2025-10-12",
-  },
-];
+let todoList = JSON.parse(localStorage.getItem("todoList")) || [];
 
 renderTodoList();
 
@@ -18,7 +12,7 @@ function renderTodoList() {
     const html = `
     <div>${name}</div> 
     <div> ${dueDate}</div>
-     <button class="deleteBtn" onclick="todoList.splice(${i}, 1); renderTodoList()">Delete</button>`;
+     <button class="deleteBtn" onclick="todoList.splice(${i}, 1); renderTodoList(); saveStorage()" >Delete</button>`;
     todoListHtml += html;
   }
 
@@ -31,14 +25,36 @@ function addTodo() {
   const dateInputElement = document.querySelector(".js-dueDate-input");
   const dueDate = dateInputElement.value;
 
-  todoList.push({ name, dueDate });
+  verifyEmptyInput(name, dueDate);
+  if (name.trim() === "" || dueDate.trim() === "") {
+    return;
+  } else {
+    todoList.push({ name, dueDate });
 
-  inputElement.value = "";
-  renderTodoList();
+    inputElement.value = "";
+    saveStorage();
+
+    renderTodoList();
+  }
 }
 
 function pressEnter(event) {
   if (event.key === "Enter") {
     addTodo();
+    saveStorage();
+  }
+}
+
+function saveStorage() {
+  localStorage.setItem("todoList", JSON.stringify(todoList));
+}
+
+function verifyEmptyInput(name, dueDate) {
+  const error = document.querySelector(".js-error");
+
+  if (name.trim() === "" || dueDate.trim() === "") {
+    error.innerHTML = "Preencha todos os campos.";
+  } else {
+    error.innerHTML = "";
   }
 }
