8c44b050f704e5e44b182dbc003910ddf8398b0d
Thgzin <thgziin@outlook.com>
2025-05-17 19:51:06 -0300

    forEach

---
+++ section12/scripts/todo-list.js
@@ -0,0 +1,58 @@
+let todoList = JSON.parse(localStorage.getItem("todoList")) || [];
+
+renderTodoList();
+
+function renderTodoList() {
+  let todoListHtml = "";
+
+  todoList.forEach((todoObject, index) => {
+    const { name, dueDate } = todoObject;
+    const html = `
+    <div>${name}</div> 
+    <div> ${dueDate}</div>
+     <button class="deleteBtn" onclick="todoList.splice(${index}, 1); renderTodoList(); saveStorage()" >Delete</button>`;
+    todoListHtml += html;
+  });
+
+  document.querySelector(".js-todo-list").innerHTML = todoListHtml;
+}
+
+function addTodo() {
+  const inputElement = document.querySelector(".todo-input");
+  const name = inputElement.value;
+  const dateInputElement = document.querySelector(".js-dueDate-input");
+  const dueDate = dateInputElement.value;
+
+  verifyEmptyInput(name, dueDate);
+  if (name.trim() === "" || dueDate.trim() === "") {
+    return;
+  } else {
+    todoList.push({ name, dueDate });
+
+    inputElement.value = "";
+    saveStorage();
+
+    renderTodoList();
+  }
+}
+
+function pressEnter(event) {
+  if (event.key === "Enter") {
+    addTodo();
+    saveStorage();
+  }
+}
+
+function saveStorage() {
+  localStorage.setItem("todoList", JSON.stringify(todoList));
+}
+
+function verifyEmptyInput(name, dueDate) {
+  const error = document.querySelector(".js-error");
+
+  if (name.trim() === "" || dueDate.trim() === "") {
+    error.innerHTML = "Preencha todos os campos.";
+  } else {
+    error.innerHTML = "";
+  }
+}
