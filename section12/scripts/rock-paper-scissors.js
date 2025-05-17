bc3de03f1b8731c0045a905d1030cc2c26c196fc
Thgzin <thgziin@outlook.com>
2025-05-17 19:33:18 -0300

    button-setInterval

---
+++ section12/scripts/rock-paper-scissors.js
@@ -0,0 +1,106 @@
+let score = JSON.parse(localStorage.getItem("score")) || {
+  wins: 0,
+  losses: 0,
+  ties: 0,
+};
+
+updateScoreElement();
+
+function resetScore() {
+  (score.wins = 0),
+    (score.losses = 0),
+    (score.ties = 0),
+    localStorage.removeItem("score");
+
+  updateScoreElement();
+  document.querySelector(".js-result").innerHTML = "";
+  document.querySelector(".js-moves").innerHTML = "";
+}
+
+function playGame(playerMove) {
+  computerMove = pickComputerMove();
+  result = "";
+
+  if (playerMove === "scissors") {
+    if (computerMove === "rock") {
+      result = "You Lose.";
+    } else if (computerMove === "paper") {
+      result = "You Win.";
+    } else if (computerMove === "scissors") {
+      result = "Tie.";
+    }
+  } else if (playerMove === "paper") {
+    if (computerMove === "rock") {
+      result = "You Win.";
+    } else if (computerMove === "paper") {
+      result = "Tie.";
+    } else if (computerMove === "scissors") {
+      result = "You Lose.";
+    }
+  } else if (playerMove === "rock") {
+    if (computerMove === "rock") {
+      result = "Tie.";
+    } else if (computerMove === "paper") {
+      result = "You Lose.";
+    } else if (computerMove === "scissors") {
+      result = "You Win.";
+    }
+  }
+
+  if (result === "You Win.") score.wins += 1;
+  else if (result === "You Lose.") score.losses += 1;
+  else if (result === "Tie.") score.ties += 1;
+
+  localStorage.setItem("score", JSON.stringify(score));
+
+  updateScoreElement();
+  updateResult(result);
+  updateMove(playerMove, computerMove);
+}
+
+function pickComputerMove() {
+  const randomNumber = Math.random();
+  let computerMove = "";
+  if (randomNumber >= 0 && randomNumber < 1 / 3) {
+    computerMove = "rock";
+  } else if (randomNumber >= 1 / 3 && randomNumber < 2 / 3) {
+    computerMove = "paper";
+  } else if (randomNumber >= 2 / 3 && randomNumber < 1) {
+    computerMove = "scissors";
+  }
+  return computerMove;
+}
+
+let isAutoPlay = false;
+let intervalId;
+
+function autoPlay() {
+  if (!isAutoPlay) {
+    intervalId = setInterval(() => {
+      const computerMove = pickComputerMove();
+      playGame(computerMove);
+    }, 1000);
+    isAutoPlay = true;
+  } else {
+    clearInterval(intervalId);
+    isAutoPlay = false;
+  }
+}
+
+function updateScoreElement() {
+  document.querySelector(
+    ".js-score"
+  ).innerHTML = ` Wins: ${score.wins}, Losses: ${score.losses}, Ties: ${score.ties}`;
+}
+
+function updateResult(result) {
+  document.querySelector(".js-result").innerHTML = result;
+}
+
+function updateMove(playerMove, computerMove) {
+  document.querySelector(
+    ".js-moves"
+  ).innerHTML = `You <img src="images/${playerMove}-emoji.png" class="move-icon" />
+    <img src="images/${computerMove}-emoji.png" class="move-icon" />Computer
+  </p>`;
+}
